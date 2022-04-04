3d02b0f6c85ed4e16b21e95a8d6fb7c370aae55b
blood72 <blood72@kakao.com>
2022-04-05 04:08:45 +0900

    소스 최상위 경로 alias 지정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8000,
+  },
+  alias: {
+    '@': path.resolve(__dirname, './src'),
   },
   plugins: [vue()],
 })
